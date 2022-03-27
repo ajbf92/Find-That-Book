7fd30c300da14e1ea01c49ecbf1145da6d0c8c02
ajbf92 <92046694+ajbf92@users.noreply.github.com>
2022-03-27 17:32:48 -0400

    deploying app

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
+
+if (process.env.NODE_ENV === "development"){require('dotenv').config();}
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
